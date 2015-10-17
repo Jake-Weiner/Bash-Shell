051621b5c2548b75f24c28441ab14800bb50d7a3
Jake Weiner <jakeweiner@hotmail.com>
2015-10-17 21:00:06 +0800

    parser for the path locations

---
+++ path-parser.c
@@ -0,0 +1,21 @@
+//
+//  path-parser.c
+//  
+//
+//  Created by Jake Weiner on 16/10/2015.
+//
+//
+
+#include <stdio.h>
+
+int main(argc, char *argv[])
+{
+    char string_buff[MAX_PATH_LENGTH];
+    char **paths;
+    
+    for (int i=0; i<)
+    
+    
+    return 0;
+}
+
