18edd2d77ac29f3fd0870ae0594c330f2a9adebb
bslqy <853185345@qq.com>
2015-10-20 17:10:22 +0800

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,10 +1,10 @@
 #include "mysh.h"
-// cc -std=c99 -Wall -Werror -pedantic execute.c globals.c mysh.c parser.c
+
 /*
-   CITS2002 Project 2 2015
-   Name(s):		student-name1 (, student-name2)
-   Student number(s):	student-number-1 (, student-number-2)
-   Date:		date-of-submission
+ CITS2002 Project 2 2015
+ Name(s):		student-name1 (, student-name2)
+ Student number(s):	student-number-1 (, student-number-2)
+ Date:		date-of-submission
  */
 
 // -------------------------------------------------------------------
@@ -15,74 +15,84 @@
 
 int execute_cmdtree(CMDTREE *t)
 {
+    printf("%s",PATH);
     int  exitstatus;
-
+    
     if(t == NULL)
     {			// hmmmm, a that's problem
-	exitstatus	= EXIT_FAILURE;
+        exitstatus = EXIT_FAILURE;
+        exit(exitstatus);
+
     }
-    else { 
-        
-    if(strncmp (t->argv[0] , "exit" , 4) == 0) //exit command
-        {
-            exit(0);
-        }
-       
-            int  pid;
-            
-            switch (pid = fork()) {
-                case -1 :
-                    perror("fork() failed");     // process creation failed
-                    exit(1);
-                    break;
-                    
-                case 0:// new child process
-                    if(strncmp (t->argv[0] , "cd" , 2) == 0) // attempt to change directory
-                    {
-                        if(t->argc == 2) // default path HOME
-                        {
-                            chdir (HOME);
-                            perror("change directory fails");
-                            exit(EXIT_FAILURE);
-                        }
-                        
-                     else  if(t->argc == 3 && (strchr(t->argv[0],'/')) == NULL ) // Given a path without '/'
-                        {
-                            chdir (t->argv[1]);
-                            perror("change directory fails");
-                            exit(EXIT_FAILURE);
-                        }
-                        else                     // Given a full path
-                        {
-                            chdir (t->argv[1]);
-                            perror("change directory fails");
-                            exit(EXIT_FAILURE);
-                        }
-                        
-                    }
  
-                    if ((strchr(t->argv[0],'/')) == NULL )  // normal command without '/'
-                    {
+    if((strncmp (t->argv[0] , "exit" , 4) == 0)&& t->argc == 1)//exit command
+        {
+            exitstatus = EXIT_SUCCESS;
+            exit(exitstatus);
+        }
+    
+    if((strncmp (t->argv[0] , "exit" , 4) == 0)&& t->argc != 1)
+    {
+        exitstatus = atoi(t->argv[1]);
+      //  printf(" %d",t->argc);
+      //  printf("exit  %d",atoi(t->argv[1]));
+        exit(exitstatus);
+    }
+        
+        if(strncmp (t->argv[0] , "cd" , 2) == 0) // attempt to change directory
+        {
+            if(t->argc == 1) // default path HOME
+            {
+                chdir ("bin/ls");
+                perror("change directory1 fails");
+                exit(EXIT_FAILURE);
+            }
+            
+            else  if(t->argc == 2 && (strchr(t->argv[1],'/')) == NULL ) // Given a path without '/'
+            {
+                chdir (t->argv[1]);
+                perror("change directory2 fails");
+                exit(EXIT_FAILURE);
+            }
+            else if(t->argc == 2 && (strchr(t->argv[1],'/')) != NULL )            // Given a full path
+            {
+                chdir (t->argv[1]);
+                perror("change directory3 fails");
+                exit(EXIT_FAILURE);
+            }
+            
+        }
+
+        
+        int  pid;
+        
+        switch (pid = fork()) {
+            case -1 :
+                perror("fork() failed");     // process creation failed
+                exit(1);
+                break;
+                
+            case 0:// new child process
+                
+                if ((strchr(t->argv[0],'/')) == NULL )  // normal command without '/'
+                {
                     execvp(t->argv[0],t->argv);
                     exit(EXIT_FAILURE);
-                    }
-                    else                      // normal command with '/'
-                    {
-                        execv(t->argv[0],t->argv);
-                        exit(EXIT_FAILURE);
-                    }
-                    
-                  default:                  // original parent process
-                    while(wait(&exitstatus) != pid);
-                    
-                    break;
-            }
-            fflush(stdout);
-       
+                }
+                else                      // normal command with '/'
+                {
+                    execv(t->argv[0],t->argv);
+                    exit(EXIT_FAILURE);
+                }
+                
+            default:                  // original parent process
+                while(wait(&exitstatus) != pid);
+                
+                break;
+        }
+        fflush(stdout);
         
-        // normal, exit commands
-	exitstatus	= EXIT_SUCCESS;
-    }
-
+    
+    
     return exitstatus;
 }
