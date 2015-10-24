9340100d831bd5d73641f1a30926420a40bf7825
bslqy <853185345@qq.com>
2015-10-24 14:35:07 +0800

    Update execute.c

--- execute.c
+++ execute.c
@@ -2,9 +2,9 @@
 
 /*
  CITS2002 Project 2 2015
- Name(s):		student-name1 (, student-name2)
- Student number(s):	student-number-1 (, student-number-2)
- Date:		date-of-submission
+ Name(s):       student-name1 (, student-name2)
+ Student number(s): student-number-1 (, student-number-2)
+ Date:      date-of-submission
  */
 // cc -std=c99 -Wall -Werror -pedantic execute.c globals.c mysh.c parser.c
 // -------------------------------------------------------------------
@@ -47,10 +47,11 @@ void pathParser (char *string)
 int execute_cmdtree(CMDTREE *t)
 {
     int  exitstatus =0;
+    int pid;
 
     if (t == NULL)
-    {			// hmmmm, a that's problem
-        exitstatus	= EXIT_FAILURE;
+    {           // hmmmm, a that's problem
+        exitstatus  = EXIT_FAILURE;
     }
  // ---------------------exit execution
             if(strncmp (t->argv[0] , "exit" , 4) == 0) //exit command
@@ -64,22 +65,70 @@ int execute_cmdtree(CMDTREE *t)
                     exit(exitstatus);
                 }
 
-
             }
+
+             if ((strchr(t->argv[0],'/')) != NULL )  // normal command with '/'
+            {
+                                switch (pid = fork()) {
+                                    case -1 :
+                                        perror("fork() failed");     // process creation failed
+                                        exit(1);
+                                        break;
+
+                                    case 0:// a new child process is created
+                                            execv(t->argv[0],t->argv);
+                                            exit(EXIT_FAILURE);
+                                            break;
+                                    default:                      // original parent process
+                                        while(wait(&exitstatus) != pid); // waits for the child process to finish running;
+                                        break;
+                                }
+                            fflush(stdout);
+                }
+
+            if ((strchr(t->argv[0],'/')) == NULL )  // normal command without '/'
+            {
+                                switch (pid = fork()) {
+                                    case -1 :
+                                        perror("fork() failed");     // process creation failed
+                                        exit(1);
+                                        break;
+
+                                    case 0:// a new child process is created
+                                            pathParser(PATH);  //parses PATH into an array
+                                            for (int i=0; i<CURRENT_NO_OF_DIRECTORIES ; i++)
+                                            {
+                                                strcat(DIRECTORIES[i],"/");              // appending a back slash for every directory
+                                                strcat(DIRECTORIES[i], t->argv[0]) ;        // appending the directory with user command to form a complete direcotry
+
+                                                if (execv(DIRECTORIES[i], t->argv) == -1)    // keep trying different directories until successful
+                                                {
+                                                    continue;
+                                                }
+                                                break;          // stop trying once succeeds
+                                            }
+
+                                    default:                      // original parent process
+                                        while(wait(&exitstatus) != pid); // waits for the child process to finish running;
+                                        break;
+                                }
+                            fflush(stdout);
+                }
 
     // --------------- cd execution
 
-                int  pid;
-
-                switch (pid = fork()) {
+if(strncmp (t->argv[0] , "cd" , 2) == 0) 
+{
+                switch (pid = fork()) 
+                {
                     case -1 :
                         perror("fork() failed");     // process creation failed
                         exit(1);
                         break;
 
                     case 0:// new child process
-                            if(strncmp (t->argv[0] , "cd" , 2) == 0) // if the command is cd then follow these conditions
-                            {
+                             // if the command is cd then follow these conditions
+                            
                                 if(t->argc == 1) // No path is specified so the directory is changed to the default HOME directory
                                 {
                                     chdir (HOME);
@@ -98,7 +147,7 @@ int execute_cmdtree(CMDTREE *t)
                                         strcat(current_working_directory,t->argv[1]);
 
                                         if( chdir(current_working_directory) ==-1)
-                                        { 	perror("ERROR");
+                                        {   perror("ERROR");
                                             exit(EXIT_FAILURE);
                                         }
                                     }
@@ -107,53 +156,16 @@ int execute_cmdtree(CMDTREE *t)
 
                                     default:                  // original parent process
                                         while(wait(&exitstatus) != pid);
+                                        if(exitstatus == 0)
+                                        {
+                                            printf("successful");
+                                        }
                                         break;
-
-
-                                }
-
+            }
+            fflush(stdout);
+    }
 
  // ------------normal command execution
 
-                                switch (pid = fork()) {
-                                    case -1 :
-                                        perror("fork() failed");     // process creation failed
-                                        exit(1);
-                                        break;
-
-                                    case 0:// a new child process is created
-
-                                        if ((strchr(t->argv[0],'/')) == NULL )  // normal command without '/'
-                                        {
-                                            pathParser(PATH);  //parses PATH into an array
-                                            for (int i=0; i<CURRENT_NO_OF_DIRECTORIES ; i++)
-                                            {
-                                                strcat(DIRECTORIES[i],"/");              // appending a back slash for every directory
-                                                strcat(DIRECTORIES[i], t->argv[0]) ;        // appending the directory with user command to form a complete direcotry
-
-                                                if (execv(DIRECTORIES[i], t->argv) == -1)    // keep trying different directories until successful
-                                                {
-                                                    continue;
-                                                }
-                                                break;          // stop trying once succeeds
-                                            }
-
-                                        }
-                                        else                      // normal command with '/'
-                                        {
-                                            execv(t->argv[0],t->argv);
-                                            exit(EXIT_FAILURE);
-                                        }
-
-
-                                    default:					  // original parent process
-                                        while(wait(&exitstatus) != pid); // waits for the child process to finish running;
-                                        break;
-                                }
-
-                                // normal, exit commands
-                                exitstatus	= EXIT_SUCCESS;
-
-                            return exitstatus;
-                }
+                return exitstatus;
 }
