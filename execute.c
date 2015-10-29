4049e421ffa36039be7f5de4dd8f1a54dd24ea06
bslqy <853185345@qq.com>
2015-10-29 21:39:10 +0800

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,4 +1,5 @@
 #include "mysh.h"
+#include "errno.h"
 
 /*
  CITS2002 Project 2 2015
@@ -17,22 +18,21 @@
 //  THAT IT HOLDS, RETURNING THE APPROPRIATE EXIT-STATUS.
 //  READ print_cmdtree0() IN globals.c TO SEE HOW TO TRAVERSE THE COMMAND-TREE
 // -------------------change directory
-int start_time_sec ;
-int start_time_usec;
-int stop_time_sec;
-int stop_time_usec;
-int  exitstatus =0;
+
+int  exitstatus = 0;
 
 int timeCommand(CMDTREE *t);
 int exitCommand(CMDTREE *t);
 int cdCommand(CMDTREE *t);
-int specifiedInternalCommand(char **latest_argv);    
-int unspecifiedInternalCommand(char **latest_argv);
+int specifiedInternalCommand(CMDTREE *t);    
+int unspecifiedInternalCommand(CMDTREE *t);
+int set_argument(CMDTREE *t);
 int do_N_COMMAND(CMDTREE *t);
 int do_N_SEMICOLON (CMDTREE *t);
 int do_N_AND(CMDTREE *t);
 int do_N_OR(CMDTREE *t);
 int do_N_PIPE(CMDTREE *t);
+
 
 
 int do_N_SEMICOLON(CMDTREE *t)
@@ -129,45 +129,56 @@ int do_N_SUBSHELL (CMDTREE *t)
 int do_N_PIPE(CMDTREE *t)
 {
 
- int pid;
- int pipefd[2];
+ int pid;   //FIRST CMD
+ int pid2; // SECOND CMD
+ int pipefd[2]; //ONE PIPE
 
 
     switch (pid = fork())
     {
         case -1 :
-            perror("fork() failed");     // process creation failed
+            perror("fork() failed");     // PROCESS CREATION FAILS
         return exitstatus;
             
         case 0 :     // CHILD PROCESS
-        if (pipe(pipefd) == -1)
-         {
-        perror("pipe");
+         if (pipe(pipefd) == -1)
+        {
+        perror("pipe failed\n");
         exit(EXIT_FAILURE);
         }
 
-        dup2(pipefd[1],STDOUT_FILENO);
-        close(pipefd[1]);
-        close(pipefd[0]);
-         // out end of the pipe is connnected to the stdout of cmd1 
-        //exitstatus = execute_cmdtree(t->left);
-        execvp(t->argv[0],t->argv);
-        exit(EXIT_FAILURE);
+        switch(pid2 = fork())
+        {
+          case -1 :
+          perror("fork() failed");
+          return exitstatus;
 
-        default :
-         
+          case 0:
+          close(pipefd[1]);
+          //close(STDIN_FILENO);
+          dup2(pipefd[0],STDIN_FILENO);
+          close(pipefd[0]);
+          exitstatus = execute_cmdtree(t->right);
+          exit(EXIT_FAILURE);
+
+          default :
         close(pipefd[0]);
-        close(pipefd[1]); 
+        dup2(pipefd[1],STDOUT_FILENO);
+         // out end of the pipe is connnected to the stdout of cmd1 
+        exitstatus = execute_cmdtree(t->left);
+        exit(EXIT_SUCCESS);
+          //while(wait(&exitstatus) != pid2); 
+          break;
+        }
+         
+        default :
         
-        // in end of the pipe is connnected to the stdout of cmd1 
-        while(wait(&exitstatus) != pid); // waits for the child process to finish running;      
-        
+         while(wait(&exitstatus) != pid); // waits for the child process to finish running;   
         break;
         exit(EXIT_FAILURE);           
     }
-    //exitstatus = execute_cmdtree(t->right);
-    execvp(t->argv[0],t->argv);
-    exit(EXIT_FAILURE);    
+   
+      
   return exitstatus;
 }
 
@@ -330,43 +341,21 @@ int timeCommand(CMDTREE *t)
                 if ((strchr(t->argv[1],'/')) == NULL ) // if the command given does not have specified location
                 {
                     
-                   exitstatus = unspecifiedInternalCommand(tempargv);
+                   exitstatus = unspecifiedInternalCommand(t);
                 }
         
                 else
                 {
-                    exitstatus = specifiedInternalCommand(tempargv); //  if the command given does have specified location
+                    exitstatus = specifiedInternalCommand(t); //  if the command given does have specified location
                 }  
                gettimeofday(&stop_time, NULL);
                 stop_time_sec = stop_time.tv_sec;
                 stop_time_usec = stop_time.tv_usec;
                 printf("program used  %d ms\n" ,(stop_time_sec - start_time_sec) *1000 + (stop_time_usec -start_time_usec)/1000);
-                exit(EXIT_FAILURE);
-                
-                
+                exit(EXIT_FAILURE);           
             default:                      // original parent process
                 
                 while(wait(&exitstatus) != pid); // waits for the child process to finish running;
-                
-        
-                /*printf("program stopped at %Lf %Lf\n",
-                 stop_time_sec, stop_time_usec );
-                 fprintf(stderr,"program ran for %Lf",stop_time_sec - start_time_sec + ((stop_time_usec - start_time_usec)*0.000001));
-                 */
-                
-                /*gettimeofday(&stop_time, NULL );
-                 
-                 
-                 stop_time_sec = (int)stop_time.tv_sec;
-                 stop_time_usec = (int)stop_time.tv_usec;
-                 
-                 printf("program stopped at %i.06%i\n",
-                 stop_time_sec, stop_time_usec );
-                 
-                 printf("%d\n",stop_time_sec);
-                 printf("%d\n",start_time_sec);
-                 */
-                 
                 break;
         }
          return exitstatus;
@@ -375,95 +364,203 @@ int timeCommand(CMDTREE *t)
     
 }
          
-int specifiedInternalCommand(char **latest_argv) //specified location of internal command such as ls
+int specifiedInternalCommand(CMDTREE *t) //specified location of internal command such as /bin/ls
 {
-  
    int pid;
+   int fd;
    switch (pid = fork())
+   {
+    case -1:
+    perror("fork() failed");     // process creation failed
+    exit(EXIT_FAILURE);
+    
+  case 0:// a new child process is created
+          
+      if (t->infile != NULL )  // the infile is used as the input instead of stdin
+      {
+          fd = open(t-> infile, O_RDONLY);
+          if (fd == -1)
+              perror ("file opened");  // error message for opening file
+          dup2(fd, 0);
+          if (close (fd) == -1)
+              perror ("file closed");   // error message for closing file-descriptor
+      }
+      
+      if ((t->outfile != NULL) && (t->append == false ))  // the outfile is used as the output instead of stdout
+      {
+          fd = open(t-> outfile, O_WRONLY | O_TRUNC | O_CLOEXEC |O_CREAT, S_IWUSR |S_IRUSR | S_IXUSR);
+          if (fd == -1)
+              perror ("file opened");  // error message for opening file
+          dup2(fd,1);
+          if (close(fd) ==-1)
+              perror ("file closed");   // error message for closing file-descriptor
+      }
+      
+      if ((t->outfile != NULL) && (t->append == true))
+      {
+          fd = open(t-> outfile, O_WRONLY |O_APPEND|O_CREAT, S_IWUSR |S_IRUSR | S_IXUSR);
+          if (fd == -1)
+              perror ("file opened");  // error message for opening file
+          dup2(fd,1);
+          if (close(fd) ==-1)
+              perror ("file closed");   // error message for closing file-descriptor
+      }
+      
+      if (access(t->argv[0], F_OK | R_OK | X_OK)==0)
+      {
+          exitstatus = execv(t->argv[0],t->argv); // EXECUTE THE SYSTEM CALL
+          
+          // ----------------------------------- if the system call fails but the file does it exist, a new copy of mysh is executed with the input changed from stdin to the shellscript file found
+          FILE *fp =fopen(t->argv[0],"r");
+          while(!feof(fp))
           {
-                                    case -1 :
-                                        perror("fork() failed");     // process creation failed
-                                        exit(EXIT_FAILURE);
-                                        
-                                        
-                                    case 0:// a new child process is created
-                                    execv(latest_argv[0],latest_argv);
-                                    exit(EXIT_FAILURE);
+              CMDTREE *t1 = parse_cmdtree(fp);
+              if(t1 != NULL)
+              {
+                  exitstatus = execute_cmdtree(t1);
+                  free_cmdtree(t1);
+              }
+          }
+          fclose(fp);
+      }
+      break;
+      exit(EXIT_FAILURE);
 
-                                    default:                      // original parent process
-                                        while(wait(&exitstatus) != pid); // waits for the child process to finish running;
-                                        break;
-            }
-                                fflush(stdout);
-                                 return exitstatus;    
+      default:                      // original parent process
+      while(wait(&exitstatus) != pid); // waits for the child process to finish running;
+      break;
+   }
+    fflush(stdout);
+    printf("exitstatus is %d\n", exitstatus);
+    return exitstatus;
 }
        
-int unspecifiedInternalCommand(char **latest_argv) //unspecified location of internal command such as ls
+int unspecifiedInternalCommand(CMDTREE *t) //unspecified location of internal command such as ls
 {
-       char *pathlist[10];
+       char *pathlist[10]; // need to change this
        int pid;
-  
-                         switch (pid = fork()) 
-                                 {
-                                    case -1 :
-                                        perror("fork() failed");     // process creation failed
-                                        exit(1);
-                                        break;
-                                        
-                                    case 0:// a new child process is created
-                                        if(PATH == NULL)
-                                        {
-                                          perror("Path is null");
-                                          exit(EXIT_FAILURE);
-                                        }
-                                        char *token = strtok(PATH,":"); //SEPERATE THE PATH VARIABLE
-                                        int n = 0;
-                                        while(token !=NULL)
-                                        {
-                                          pathlist[n] = strdup(token);   
-                                          token = strtok(NULL,":"); 
-                                          strcat(pathlist[n],"/");   //APPEND '/' FOR THE PATH
-                                          strcat(pathlist[n],latest_argv[0]); // APPEND INPUT ARGUMENT FOR THE PATH
-                                          execv(pathlist[n],latest_argv); // EXECUTE THE SYSTEM CALL
-                                          n++;
-                                        } 
-                                        exit(EXIT_FAILURE); 
+       int fd;
 
-                                    default:                      // original parent process
-                                        while(wait(&exitstatus) != pid); // waits for the child process to finish running;
-                                        break;
-                                }
-                                fflush(stdout);
-                                return exitstatus;
+   
+    switch (pid = fork())
+    {
+        case -1 :
+            perror("fork() failed");     // process creation failed
+            exit(1);
+            break;
+            
+        case 0:// a new child process is created
+            if (t->infile != NULL )  // the infile is used as the input instead of stdin
+            {
+                fd = open(t-> infile, O_RDONLY);
+                if (fd == -1)
+                    perror ("file opened");  // error message for opening file
+                dup2(fd, 0);
+                if (close (fd) == -1)
+                    perror ("file closed");   // error message for closing file-descriptor
+            }
+            
+            if ((t->outfile != NULL) && (t->append == false ))  // the outfile is used as the output instead of stdout
+            {
+                fd = open(t-> outfile, O_WRONLY | O_TRUNC | O_CLOEXEC |O_CREAT, S_IWUSR |S_IRUSR | S_IXUSR);
+                if (fd == -1)
+                    perror ("file opened");  // error message for opening file
+                dup2(fd,1);
+                if (close(fd) ==-1)
+                    perror ("file closed");   // error message for closing file-descriptor
+            }
+            
+            if ((t->outfile != NULL) && (t->append == true))
+            {
+                fd = open(t-> outfile, O_WRONLY |O_APPEND|O_CREAT, S_IWUSR |S_IRUSR | S_IXUSR);
+                if (fd == -1)
+                    perror ("file opened");  // error message for opening file
+                dup2(fd,1);
+                if (close(fd) ==-1)
+                    perror ("file closed");   // error message for closing file-descriptor
+            }
+         
+            if(PATH == NULL)
+            {
+                perror("Path is null");
+                exit(EXIT_FAILURE);
+            }
+            char *token = strtok(PATH,":"); //SEPERATE THE PATH VARIABLE
+            int n = 0;
+            
+            while(token !=NULL)
+            {
+                pathlist[n] = strdup(token);
+                token = strtok(NULL,":");
+                strcat(pathlist[n],"/");   //APPEND '/' FOR THE PATH
+                strcat(pathlist[n],t->argv[0]); // APPEND INPUT ARGUMENT FOR THE PATH
+                if (access(pathlist[n], F_OK | R_OK | X_OK)==0)
+                {
+                    execv(pathlist[n],t->argv); // EXECUTE THE SYSTEM CALL
+                    FILE *fp =fopen(pathlist[n],"r");
+                    while(!feof(fp))
+                    {
+                    CMDTREE *t1 = parse_cmdtree(fp);
+                    if(t1 != NULL)
+                        {
+                            print_cmdtree(t1);
+                            exitstatus = execute_cmdtree(t1);
+                            free_cmdtree(t1);
+                        }
+                    }
+                    fclose(fp);
+                }
+                n++;
+            }
+            perror("mysh command error");
+            exitstatus = EXIT_FAILURE;
+            exit(EXIT_FAILURE);
+            
+        default:                      // original parent process
+            while(wait(&exitstatus) != pid); // waits for the child process to finish running;
+            break;
+    }
+        fflush(stdout);
+      printf("exitstatus is %d\n", exitstatus);
+        return exitstatus;
 }
+
+int set_argument(CMDTREE *t)
+{
+  if(strcmp(t->argv[1],"PATH") == 0)
+    {
+      realloc(PATH,sizeof(char) *strlen(t->argv[2]) +1);
+      strcpy(PATH,t->argv[2]);
+      printf("PATH after set %s\n",PATH);
+      free(PATH);
+    }
+            
+  else if(strcmp(t->argv[1],"CDPATH" )== 0)
+    {
+      realloc(CDPATH,sizeof(char) *strlen(t->argv[2]) +1);
+      strcpy(CDPATH,t->argv[2]);
+      printf("CDPATH after set %s\n",CDPATH);
+      free(CDPATH);
+    }
+  else if(strcmp(t->argv[1],"HOME" )==0)
+    {
+      realloc(HOME,sizeof(char) *strlen(t->argv[2]) +1);
+      strcpy(HOME,t->argv[2]);
+      printf("HOME after set %s\n",HOME);
+      free(CDPATH);
+    }
+   else
+    {
+      fprintf(stderr, "Variable name can only be 'CDPATH','PATH','HOME' ");
+      exitstatus = EXIT_FAILURE;
+    }
+                return exitstatus;
+}
+
 
 int do_N_COMMAND(CMDTREE *t) // EXECUTION IF TYPE IS N_COMMAND
 {
-    int fd;
 
-       if (t->infile != NULL )  // the infile is used as the input instead of stdin
-       {
-                fd = open(t-> infile, O_RDONLY);
-                dup2(fd, 0);
-                close (fd);
-       }
-    
-        if ((t->outfile != NULL) && (t->append == false ))  // the outfile is used as the output instead of stdout
-        {
-            fd = open(t-> outfile, O_WRONLY | O_TRUNC | O_CLOEXEC |O_CREAT, S_IWUSR |S_IRUSR | S_IXUSR);
-            printf("open return value is %d\n" , fd);
-            dup2(fd,1);
-            close(fd);
-        }
-    
-      if ((t->outfile != NULL) && (t->append == true))
-        {
-            fd = open(t-> outfile, O_WRONLY |O_APPEND|O_CREAT, S_IWUSR |S_IRUSR | S_IXUSR);
-            printf("open return value is %d\n" , fd);
-            dup2(fd,1);
-            close(fd);
-        }
-            
             if(strcmp (t->argv[0] , "exit")== 0)
             {
                 exitstatus = exitCommand(t);
@@ -473,24 +570,40 @@ int do_N_COMMAND(CMDTREE *t) // EXECUTION IF TYPE IS N_COMMAND
             {
                 exitstatus =cdCommand(t);
             }
-            
-            // ---------------------------------ls type commands
-            else   if ((strchr(t->argv[0],'/')) != NULL ) // INPUT ARGUMENT HAS '/'
-            {
-                exitstatus = specifiedInternalCommand(t->argv);
-            }
-            else if ((strchr(t->argv[0],'/')) == NULL && strcmp (t->argv[0],"cd")!= 0 && strcmp (t->argv[0],"time")!= 0)
-            {
-                exitstatus =unspecifiedInternalCommand(t->argv);
-            }
             else if(strcmp (t->argv[0],"time")== 0) // time command
             {
             
                 exitstatus = timeCommand(t);
             }
-        
 
-            return exitstatus;
-    exit(EXIT_FAILURE);
+            else if (strcmp(t->argv[0] , "set" ) == 0) //SET ARGUMENTS
+
+              {
+                if(t->argc == 3)
+                {
+                  exitstatus = set_argument(t);
+                }
+                else 
+                {
+                  fprintf(stderr, "Insufficient Number of Arguments. Usage: set VARIABLE PATHLIST \n");
+                  exitstatus = EXIT_FAILURE;
+                }   
+              } 
+            // ---------------------------------ls type commands
+            else   if ((strchr(t->argv[0],'/')) != NULL ) // INPUT ARGUMENT HAS '/'
+            {
+                exitstatus = specifiedInternalCommand(t);
+            }
+            else if ((strchr(t->argv[0],'/')) == NULL && strcmp (t->argv[0],"cd")!= 0 && strcmp (t->argv[0],"time")!= 0 && strcmp(t->argv[0],"set")!= 0)
+            {
+
+                if((exitstatus = unspecifiedInternalCommand(t)) != 0)
+                {
+                  fprintf(stderr, "-mysh: %s:command not found\n", t->argv[0]);
+                }
+            }
+            
+              return exitstatus;
+              exit(EXIT_FAILURE);
 }
 
